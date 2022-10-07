b3e6a3551eaaf79acb3ab10029b2fd3a450c7d24
Robert Teets <teetsrobert@gmail.com>
2022-10-06 20:45:28 -0600

    2: assignment complete.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
 
 export class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    clickHandler = () => {
+        this.setState({ age: this.state.age + 1 })
+    }
+
     render() {
 
         const { firstname, lastname, age, hairColor } = this.props
@@ -8,8 +19,9 @@ export class Header extends Component {
         return (
             <div>
                 <h1>{lastname}, {firstname}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.clickHandler}>Click for Birthday!</button>
             </div>
         )
     }
